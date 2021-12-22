09eca39cb9bde444a6e805430bdc6eaf7ffcc89a
Chanqso14 <borisi.chankseliani.1@btu.edu.ge>
2021-12-22 23:26:25 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/adapters/ViewPagerFragmentAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.myapplication.fragments.FragmentFirst
+import com.example.myapplication.fragments.FragmentSecond
+import com.example.myapplication.fragments.FragmentThird
+
+class ViewPagerFragmentAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+    override fun getItemCount() = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0-> FragmentFirst()
+            1-> FragmentSecond()
+            2-> FragmentThird()
+            else -> FragmentFirst()
+        }
+    }
+}
